1018195749e30903154f59e759aebcba4ea0133c
Alethia Quintero <alethiaq22@gmail.com>
2019-07-24 23:41:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,7 +43,7 @@ class Song
 
     def Song.new_from_filename(info)
 
-      separated = info.delete([.mp3]).split("-")
+      separated = info.delete(/.mp3/).split("-")
       artist = separated[0]
       title = separated[1]
 
