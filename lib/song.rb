dd13599a611dd97cf9e81f7c32d2eb2d56ad835e
Alethia Quintero <alethiaq22@gmail.com>
2019-07-24 23:06:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,7 @@ class Song
     end
 
     def self.find_or_create_by_name(title)
-      self.find_by_name(title) || self.create_by_name
+      self.find_by_name(title) || self.create_by_name(title)
 
     end
 
